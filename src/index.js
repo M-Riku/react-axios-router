20c8b66b54f97002c3001aefc1848096c0a0e9a8
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-08 16:36:26 +0900

    Global setting in axios

--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(
+    request => {
+        //edit request config
+        return request;
+    }, error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    response => {
+        //edit response config
+        return response;
+    }, error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
