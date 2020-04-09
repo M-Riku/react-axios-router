07043a769644cd555867d293680d60550f5f29e6
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-09 21:43:13 +0900

    Add header

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -50,9 +50,16 @@ class Blog extends Component {
             );
         }
 
-
         return (
-            <div>
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
