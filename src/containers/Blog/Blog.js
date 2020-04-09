e77deb26c1b16436e237f640a9945b8979aa54ee
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-09 23:54:05 +0900

    Redirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
@@ -24,6 +24,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path='/new-post' exact component={NewPost} />
                     <Route path='/posts' component={Posts} />
+                    <Redirect from='/' to='/posts' />
                 </Switch>
             </div>
         );
